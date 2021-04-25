2def9056e48ae3b85728eeed8cc5bf2939626616
jiaxianglong <jiaxianglong@baidudeMacBook-Pro.local>
2021-04-25 13:56:51 +0800

    优化代码

---
+++ down_m3u8.sh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+
+# 开始的集数
+START_INDEX=1
+# 结束的集数
+END_INDEX=28
+
+# 线程数
+THREAD_NUM=100
+
+# 提取m3u8 url
+function get_m3u8_url() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] get_m3u8_url start"
+
+    for ((index=${START_INDEX};index<=${END_INDEX};index++)); do
+        local url=www.xjxsjyy.com/index.php/xp/yuzhaolingdiyiji-1-${index}.html
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] get_m3u8_url, index=${index}, url=${url}"
+        curl ${url} 2>&2 | grep -e "\"url\":\"\S*\",\"url_next" -o | awk -F '"url":"' '{print $2}' | awk -F '"' '{print $1}' | sed -e "s/\\\\\//\//g" >${index}.m3u8_url 2>&2 &
+    done
+
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] get_m3u8_url gettting......"
+    wait
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] get_m3u8_url finish"
+}
+
+# 真正下载m3u8文件并拼接
+function down_m3u8() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] down_m3u8 start"
+
+    for ((index=${START_INDEX};index<=${END_INDEX};index++)); do
+        local m3u8_url=$(head -n 1 ${index}.m3u8_url)
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] down_m3u8, index=${index}, m3u8_url=${m3u8_url}"
+        rm -vf ${index}.mp4
+        ffmpeg -nostdin -i ${m3u8_url} -codec copy -acodec copy -vcodec copy -threads ${THREAD_NUM} -preset ultrafast ${index}.mp4 &>down_${index}.log &
+    done
+
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] down_m3u8 downloading......"
+    wait
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] down_m3u8 finish"
+}
+
+# 主函数
+function main() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] start"
+
+    get_m3u8_url
+    down_m3u8
+
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] finish"
+}
+
+main
--- down_mp4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# 解决ffmpeg无法直接扔后台的问题
-
-in=$1
-out=$2
-
-ffmpeg -i ${in} ${out}
